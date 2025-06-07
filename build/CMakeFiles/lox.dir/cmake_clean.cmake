dcbc6b03a8607362ed0694179e9ce9768591eb42
Agha Waleed <ebicandawesome9000@gmail.com>
2025-06-07 17:44:49 +0500

    Did lots of stuff

---
+++ build/CMakeFiles/lox.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lox.dir/ast_printer.c.o"
+  "CMakeFiles/lox.dir/ast_printer.c.o.d"
+  "CMakeFiles/lox.dir/expr.c.o"
+  "CMakeFiles/lox.dir/expr.c.o.d"
+  "CMakeFiles/lox.dir/interpreter.c.o"
+  "CMakeFiles/lox.dir/interpreter.c.o.d"
+  "CMakeFiles/lox.dir/lexer.c.o"
+  "CMakeFiles/lox.dir/lexer.c.o.d"
+  "CMakeFiles/lox.dir/logging.c.o"
+  "CMakeFiles/lox.dir/logging.c.o.d"
+  "CMakeFiles/lox.dir/main.c.o"
+  "CMakeFiles/lox.dir/main.c.o.d"
+  "CMakeFiles/lox.dir/parser.c.o"
+  "CMakeFiles/lox.dir/parser.c.o.d"
+  "lox"
+  "lox.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
